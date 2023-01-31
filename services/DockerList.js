7904457b69ebd9f9e479e935fc346113f19622af
Ruslan379 <arnov@i.ua>
2023-01-31 12:33:46 +0200

    Create DockerList.js

---
+++ services/DockerList.js
@@ -0,0 +1,12 @@
+//! Установка образа версии Node.js
+//  docker pull node:16.19.0-alpine3.17
+
+//! Создание образа
+//  docker build -t hw6 .
+
+//! Запуск контейнера
+//  docker run -dp 3000:3000 ruslan379/hw6
+
+//! Push на Docker Hub
+// docker push ruslan379/hw6:latest
+
